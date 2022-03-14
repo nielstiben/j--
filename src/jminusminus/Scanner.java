2e3105cb2492537377bfc99653b654252d7078dd
AsbjornOlling <asbjornolling@gmail.com>
2022-03-14 15:07:11 +0100

    add questionmark token to scanner

--- src/jminusminus/Scanner.java
+++ src/jminusminus/Scanner.java
@@ -358,6 +358,9 @@ class Scanner {
         case ':':
             nextCh();
             return new TokenInfo(COLON, line);
+        case '?':
+            nextCh();
+            return new TokenInfo(QUESTIONMARK, line);
         case EOFCH:
             return new TokenInfo(EOF, line);
         case '0':
