66b7b8bb2a60b3c7a99b6e62cedadb3bc3030e2d
Johannes <s213578@dtu.dk>
2022-03-15 17:19:34 +0100

    added support for type Double in +=, -=, *=, /=, %=

--- tests/pass/AssignmentOperators.java
+++ tests/pass/AssignmentOperators.java
@@ -7,9 +7,20 @@ public class AssignmentOperators {
         int b = 2;
         int c = 3;
 
+        double d = 1.4;
+        double e = 2.6;
+        double f = 3.7;
+
+        a += 2;
         a -= 1;
         b *= 5;
         c /= 1;
         c %= 5;
+
+        d += 2.6;
+        d -= 1.2;
+        e *= 5;
+        f /= 1;
+        f %= 5;
     }
 }
