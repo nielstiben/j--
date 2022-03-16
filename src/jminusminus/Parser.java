5af99fe4c20b09cd6e3dd3ae7b9dd338c426a1d2
AsbjornOlling <asbjornolling@gmail.com>
2022-03-16 11:48:18 +0100

    hotfix for logical or

--- src/jminusminus/Parser.java
+++ src/jminusminus/Parser.java
@@ -1020,7 +1020,7 @@ public class Parser {
 
     private JExpression assignmentExpression() {
         int line = scanner.token().line();
-        JExpression lhs = conditionalAndExpression();
+        JExpression lhs = conditionalOrExpression();
         if (have(ASSIGN)) {
             return new JAssignOp(line, lhs, assignmentExpression());
         } else if (seeCompoundAssignmentExpression()) {
