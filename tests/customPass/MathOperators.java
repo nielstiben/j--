cbde08e0952a20e3d20e03aef4d1f253cc983755
Gustav Weber Kinch <Gustavwk@gmail.com>
2022-03-28 12:31:19 +0200

    wewe

---
+++ tests/customPass/MathOperators.java
@@ -0,0 +1,18 @@
+public class MathOperators {
+    public static void main(String[] args) {
+        double a = 50.87;
+        int b = 2;
+        a=a+a;
+        a=a-a;
+        a=a/a;
+        a=a*a;
+        a=a%a;
+
+        b=b+b;
+        b=b-b;
+        b=b/b;
+        b=b*b;
+        b=b%b;
+    
+    }
+}
