e00e249c18471f77b1b8171b210703086e723285
s213578 <s213578@dtu.dk>
2022-04-04 14:55:36 +0200

    started the ternary operator

---
+++ tests/customPass/TernaryOperator.java
@@ -0,0 +1,10 @@
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+        int x, y;
+        x = 20;
+        boolean lol = (2 == 1);
+        //y = (x == 1) ? 61 : 90;
+        
+    }
+}
