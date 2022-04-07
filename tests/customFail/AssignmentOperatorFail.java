fd85261bff7196e3f353f91525718371ef6f4c10
Gustav Weber Kinch <Gustavwk@gmail.com>
2022-04-07 14:53:00 +0200

    flere fail tests

---
+++ tests/customFail/AssignmentOperatorFail.java
@@ -0,0 +1,11 @@
+class AssignmentOperatorFail {
+public static void main(String[]args){
+        int a = 19;
+        a = "ads";
+
+        double b = 19;
+        b = "ssss";
+
+        a = b;
+        }
+}
---
+++ tests/customFail/ForEachStatementFail.java
@@ -0,0 +1,19 @@
+class ForEachStatementFail {
+    public static void main(String[] args) {
+        int [] ints = new int[10];
+        String [] strings = new String[10];
+
+        for (String s, d ,f ,g: strings) {
+
+        }
+
+        for (int nr: nr) {
+
+        }
+
+
+        for (int nr: ints) {
+            -{}-
+        }
+    }
+}
