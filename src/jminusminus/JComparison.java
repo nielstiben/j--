839bc23ff2cefb9b93ea44ca1fede75f56c184c1
Niels Tiben <nielstiben@outlook.com>
2022-04-21 16:08:32 +0200

    implement For loop analyser Made update statement in for loop optional

--- src/jminusminus/JComparison.java
+++ src/jminusminus/JComparison.java
@@ -40,7 +40,7 @@ abstract class JComparison extends JBooleanBinaryExpression {
     public JExpression analyze(Context context) {
         lhs = (JExpression) lhs.analyze(context);
         rhs = (JExpression) rhs.analyze(context);
-        lhs.type().mustMatchExpected(line(), Type.INT);
+        lhs.type().mustMatchOneOf(line(), Type.INT, Type.DOUBLE);
         rhs.type().mustMatchExpected(line(), lhs.type());
         type = Type.BOOLEAN;
         return this;
--- tests/pass/For.java
+++ tests/pass/For.java
@@ -1,27 +1,28 @@
 public class For {
     public static void main(String[] args) {
 
-        double d = 0.0;
-        int y = 0;
-
         for (int i = 0; i < 2; i++) {
-            
+            int a = 2;
         }
 
-        for (int i = 10; i > 0; i--) {
-            
+        for (int i = 10; i < 0; i--) {
+            int a = 3;
         }
 
         for (int i = 2; i < 10; i*=2) {
-            
         }
 
-        for (double i = 0.0; i < 10; i++) {
-            
+
+        for (double i = 0.0; i < 10.0; i++) {
+
         }
 
-        int a = 1;
-        String b;
-        
+        for (double q = 0.0; q < 5.5; q = q + 0.1) {
+
+        }
+
+        for (double q = 0.0; q < 5.5;) {
+            q+=0.2;
+        }
     }
 }
